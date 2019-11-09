b63d364c3e418af941581a6f7d967b7e9c8ecc7c
RationalGit <48952095+RationalGit@users.noreply.github.com>
2019-11-09 09:11:13 +0000

    new file:   data/LA_HP_I_ratio_UK.csv 	new file:   data/LA_HP_UK.csv 	new file:   data/LA_I_UK.csv 	new file:   data/LA_Med_ratio_UK.xls 	modified:   load.R 	renamed:    working.R -> working MSOA.R
    Former-commit-id: e6a54d985f072a4ae42c0aabea41d750b9dd34ca

--- load.R
+++ load.R
@@ -97,3 +97,57 @@ spdf_EW_data <- spdf_EW_simp %>%
 spdf_EW_data <- arrange(spdf_EW_data, id, order)
 
 saveRDS(spdf_EW_data, file = "spdf_EW_data.rds")
+
+## Income estimate datasets: (available from: https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/datasets/smallareaincomeestimatesformiddlelayersuperoutputareasenglandandwales)
+
+## Local authority datasets: (available from: https://www.ons.gov.uk/peoplepopulationandcommunity/housing/datasets/housepriceexistingdwellingstoresidencebasedearningsratio)
+
+
+download.file("https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fhousing%2fdatasets%2fhousepriceexistingdwellingstoresidencebasedearningsratio%2fcurrent/ratioofhousepriceexistingdwellingstoresidencebasedearningslowerquartileandmedian.xls", 
+              destfile = "LA_Med_ratio_UK.xls" , mode='wb')
+
+# Pull out xls sheet with all house data (sheet 1)
+sheet_1 <- read_xls("LA_Med_ratio_UK.xls", sheet = '1a')
+LA_sheets <- data.frame(sheet_name = c("5a", "5b", "5c"),
+                        csv_name = c("LA_HP_UK.csv", "LA_I_UK.csv", "LA_HP_I_ratio_UK.csv"),
+                        df_name = c("data_LA_HP", "data_LA_I", "data_LA_Ratio"))
+typeof(as.character(LA_sheets$sheet_name[1]))
+
+for(i in 1:3){
+  sheet_i = read_xls("LA_Med_ratio_UK.xls", sheet = as.character(LA_sheets$sheet_name[i]))
+  write.csv(sheet_i, file = as.character(LA_sheets$csv_name[i]), row.names = FALSE)
+  print(paste0(as.character(LA_sheets$csv_name[i]), " written"))
+  csv = read.csv(as.character(LA_sheets$csv_name[i]), skip = 6, header=T, na.strings = ":", check.names = F)
+  csv = csv[1:348,]
+  assign(as.character(LA_sheets$df_name[i]), csv)
+}
+  
+write.csv(sheet_1, file="MSOA_MedHP_UK.csv", row.names=FALSE)
+
+names(data_LA_HP) <- names(data_LA_I)
+df_gather <- function(df, col_name){
+  col2cvt <- 5:21
+  df[,col2cvt] <- sapply(df[col2cvt], clean)
+  df <- df %>%
+    gather(Year, !!col_name, 5:21)
+  make.names(df)
+  return(df)
+}
+data_LA_I <- df_gather(data_LA_I[1:348,], 'Income')
+data_LA_HP <- df_gather(data_LA_HP[1:348,], "House.Price")
+data_LA_Ratio <- df_gather(data_LA_Ratio[1:348,], "Ratio")
+
+data_LA <- cbind(data_LA_I, data_LA_HP[,6], data_LA_Ratio[,6])
+names(data_LA)[7:8] <- c("House.price", "Ratio")
+names(data_LA) <- make.names(names(data_LA))
+data_LA$Year <- as.numeric(data_LA$Year)
+sapply(data_LA, class)
+
+data_LA <- data.table(data_LA)
+data_LA[,list(mean=mean(Income, na.rm = T)),by=Region.name]
+data_LA$median.income <- round(ave(data_LA$Income, as.factor(data_LA$Region.name), as.factor(data_LA$Year), FUN = function(x) median(x,na.rm = T)))
+data_LA$median.HP <- round(ave(data_LA$House.price, as.factor(data_LA$Region.name), as.factor(data_LA$Year), FUN = function(x) median(x,na.rm = T)))
+data_LA$median.Ratio <- round(ave(data_LA$Ratio, as.factor(data_LA$Region.name), as.factor(data_LA$Year), FUN = function(x) median(x,na.rm = T)))
+
+setwd("C:/Users/tomwe/Dropbox (Personal)/Training/Coding/20190801 UK years to home ownership/LA data/")
+saveRDS(data_LA, file = "data_LA.rds")
